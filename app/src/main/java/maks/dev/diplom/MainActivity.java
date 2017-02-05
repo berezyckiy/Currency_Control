dd4ba393a2b053d58f7365123fc3fa7a7cc4f1b6
berezyckiy <govinmaxim@gmail.com>
2017-02-06 00:23:23 +0300

    test changes

--- app/src/main/java/maks/dev/diplom/MainActivity.java
+++ app/src/main/java/maks/dev/diplom/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int x = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        x = 20;
     }
 }
