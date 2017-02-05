6ef76f3058e23fe704c7e1abc6764467b3ba7b80
berezyckiy <govinmaxim@gmail.com>
2017-02-06 00:38:22 +0300

    Revert "test changes"
    This reverts commit dd4ba393a2b053d58f7365123fc3fa7a7cc4f1b6.

--- app/src/main/java/maks/dev/diplom/MainActivity.java
+++ app/src/main/java/maks/dev/diplom/MainActivity.java
@@ -5,12 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int x = 10;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        x = 20;
     }
 }
