fb6959700156f69bf914aaad4fa7edb76985a1a6
berezyckiy <govinmaxim@gmail.com>
2017-02-07 22:47:12 +0300

    before chosmaincurr

--- app/src/main/java/maks/dev/diplom/Fragments/MainFragment.java
+++ app/src/main/java/maks/dev/diplom/Fragments/MainFragment.java
@@ -1,23 +1,70 @@
 package maks.dev.diplom.Fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import maks.dev.diplom.CurrencyFactory;
 import maks.dev.diplom.R;
+import maks.dev.diplom.RecyclerViewAdapter;
 
 /**
  * Created by berezyckiy on 2/6/17.
  */
 
-public class MainFragment extends android.app.Fragment {
+public class MainFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    private RecyclerViewAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout swipeRefresh;
+
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        view = inflater.inflate(R.layout.main_fragment, container, false);
+        initItems();
+        buildRecycleView();
+        swipeRefresh.setOnRefreshListener(this);
+        return view;
     }
 
+    private void initItems() {
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        swipeRefresh = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefresh);
+        swipeRefresh.setColorSchemeResources(
+                android.R.color.holo_purple,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_red_dark,
+                android.R.color.holo_blue_dark,
+                android.R.color.holo_green_dark);
+    }
+
+    private void buildRecycleView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        List<CurrencyFactory.Currency> currencyList = CurrencyFactory.getCurrencyList();
+        mAdapter = new RecyclerViewAdapter(currencyList);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                swipeRefresh.setRefreshing(false);
+            }
+        }, 3000);
+    }
 }
--- app/src/main/java/maks/dev/diplom/MainActivity.java
+++ app/src/main/java/maks/dev/diplom/MainActivity.java
@@ -1,25 +1,16 @@
 package maks.dev.diplom;
 
-import android.app.FragmentTransaction;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-
-import java.util.List;
-import java.util.Random;
 
 import maks.dev.diplom.Fragments.ChooseMainCurrency;
 import maks.dev.diplom.Fragments.ChooseYourCurrency;
@@ -27,17 +18,12 @@ import maks.dev.diplom.Fragments.GrafficsFragment;
 import maks.dev.diplom.Fragments.MainFragment;
 import maks.dev.diplom.Fragments.SettingsFragment;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
-        , SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
     private NavigationView nvView;
-
-    private RecyclerViewAdapter mAdapter;
-    private RecyclerView mRecyclerView;
-    private SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,36 +33,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         nvView.setNavigationItemSelectedListener(this);
         addDrawerToggle();
-        buildRecycleView();
-        swipeRefresh.setOnRefreshListener(this);
-    }
-
-    @Override
-    public void onRefresh() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Random random = new Random();
-                TextView tv = (TextView) findViewById(R.id.tvMainScreen);
-                tv.setTextColor(Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
-                swipeRefresh.setRefreshing(false);
-            }
-        }, 2000);
-    }
-
-    private void buildRecycleView() {
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        List<CurrencyFactory.Currency> currencyList = CurrencyFactory.getCurrencyList();
-        mAdapter = new RecyclerViewAdapter(currencyList);
-        mRecyclerView.setAdapter(mAdapter);
+        startMainFragment();
     }
 
     private void initItems() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         nvView = (NavigationView) findViewById(R.id.nvView);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        swipeRefresh = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
     }
 
     private void addDrawerToggle() {
@@ -97,19 +60,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 super.onDrawerOpened(drawerView);
                 invalidateOptionsMenu();
             }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-            }
         };
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
     }
 
+    private void startMainFragment(){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        toolbar.setTitle("Currency exchange");
+        ft.replace(R.id.contentFrame, new MainFragment()).commit();
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         switch (item.getItemId()) {
             case R.id.nav_currency_exchange:
                 toolbar.setTitle("Currency exchange");
